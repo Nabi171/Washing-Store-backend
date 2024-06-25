ef133e9704211594e78cf93c7bf7eef92b67f32f
Md Nabiul Bashar <nabiultasbed@gmail.com>
2024-06-25 16:57:11 +0600

    change in booking service

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -7,21 +7,8 @@ const createBookingIntoDB = async (payload: IBooking): Promise<IBooking[]> => {
   const allBookings = [...existingBookings, newBooking];
   return allBookings;
 };
-
 const getAllBookingsFromDB = async () => {
   const result = await Booking.find();
-  return result;
-};
-
-const getSingleBookingFromDB = async (id: string) => {
-  const result = await Booking.findById(id);
-  return result;
-};
-
-const updateBookingIntoDB = async (id: string, payload: Partial<IBooking>) => {
-  const result = await Booking.findOneAndUpdate({ _id: id }, payload, {
-    new: true,
-  });
   return result;
 };
 
