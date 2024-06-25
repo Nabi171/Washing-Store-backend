731aff3b4bb4328e99354be60877eb4c4cf6c135
Md Nabiul Bashar <nabiultasbed@gmail.com>
2024-06-25 16:52:16 +0600

    change in booking controller

--- src/app/modules/booking/booking.controller.ts
+++ src/app/modules/booking/booking.controller.ts
@@ -13,7 +13,6 @@ const createBooking = catchAsync(async (req, res) => {
     data: result,
   });
 });
-
 const getAllBookings = catchAsync(async (req, res) => {
   const result = await BookingServices.getAllBookingsFromDB();
 
@@ -25,46 +24,7 @@ const getAllBookings = catchAsync(async (req, res) => {
   });
 });
 
-const getSingleBooking = catchAsync(async (req, res) => {
-  const { bookingId } = req.params;
-  const result = await BookingServices.getSingleBookingFromDB(bookingId);
-
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: "Booking is retrieved successfully",
-    data: result,
-  });
-});
-
-const updateBooking = catchAsync(async (req, res) => {
-  const { bookingId } = req.params;
-  const result = await BookingServices.updateBookingIntoDB(bookingId, req.body);
-
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: "Booking is updated successfully",
-    data: result,
-  });
-});
-
-const deleteBooking = catchAsync(async (req, res) => {
-  const { bookingId } = req.params;
-  const result = await BookingServices.deleteBookingFromDB(bookingId);
-
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: "Booking is deleted successfully",
-    data: result,
-  });
-});
-
 export const BookingControllers = {
   createBooking,
   getAllBookings,
-  getSingleBooking,
-  updateBooking,
-  deleteBooking,
 };
